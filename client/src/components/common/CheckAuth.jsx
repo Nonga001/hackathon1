782661fd021aecb858177350b67ae655c5ba14a1
Middlechild0 <jimmymathu28@gmail.com>
2024-10-05 04:24:44 +0300

    1

---
+++ client/src/components/common/CheckAuth.jsx
@@ -0,0 +1,53 @@
+import { Navigate, useLocation } from "react-router-dom";
+
+function CheckAuth(isAuthenticated, user, children) {
+  const location = useLocation();
+
+  // Redirect to login if not authenticated and not on login/register pages
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes('/login') ||
+      location.pathname.includes('/register')
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
+
+  // Redirect authenticated users based on role
+  if (
+    isAuthenticated &&
+    (location.pathname.includes('/login') ||
+      location.pathname.includes('/register'))
+  ) {
+    if (user?.role === 'admin') {
+      return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop/home" />;
+    }
+  }
+
+  // Prevent non-admin users from accessing admin routes
+  if (
+    isAuthenticated &&
+    user?.role !== 'admin' &&
+    location.pathname.includes('/admin')
+  ) {
+    return <Navigate to="/unauthorized" />;
+  }
+
+  //Directs admin to admin dashboard
+  if (
+    isAuthenticated &&
+    user?.role === 'admin'&&
+    location.pathname.includes(shop)  
+  ){
+    return <Navigate to="/admin/dashboard" />
+  }
+
+  return <>{children}</>; // Render children if no redirects
+}
+
+export default CheckAuth;
+
+
